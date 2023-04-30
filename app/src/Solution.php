8c8b3d4adee1b09b17c6ba8e9232ab3e29f8bc07
pavlik <kibuzz15@gmail.com>
2023-04-30 14:12:41 +0300

    Perfect number optimized version

--- app/src/Solution.php
+++ app/src/Solution.php
@@ -4,13 +4,18 @@ namespace App\Solution;
 
 function isPerfect(int $num): bool
 {
+    $sqrt = ceil(sqrt($num));
+    var_dump($sqrt);
     $sum = 1;
-    for ($i = 2; $i < $num; $i++) {
+    for ($i = 2; $i < $sqrt; $i++) {
         if ($num % $i == 0) {
             $sum += $i;
+            $sum += $num / $i;
+            var_dump($i);
+            var_dump($sum);
         }
     }
     return $sum == $num;
 }
 
-print_r(isPerfect(0)); // true
+print_r(isPerfect(6)); // true
